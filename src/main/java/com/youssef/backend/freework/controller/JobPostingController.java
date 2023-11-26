222eb7e92178b8a5d065c9fb4e799a9a25a290f7
yousseftalibi <yousseftalibi@outlook.com>
2023-11-26 22:45:21 +0100

    not much

--- src/main/java/com/youssef/backend/freework/controller/JobPostingController.java
+++ src/main/java/com/youssef/backend/freework/controller/JobPostingController.java
@@ -4,6 +4,7 @@ package com.youssef.backend.freework.controller;
 import com.youssef.backend.freework.model.JobPostings;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,9 +14,9 @@ public class JobPostingController {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @GetMapping("getJobs/{page}/{itemsPerPage}/{contracts}/{locationKeys}/{minDailySalary}")
-    public JobPostings getJobPostings(@RequestParam Integer page, @RequestParam Integer itemsPerPage, @RequestParam String contracts, @RequestParam String locationKeys, @RequestParam Integer minDailySalary  ) {
-        String url = "https://www.free-work.com/api/job_postings?page=".concat(String.valueOf(page)).concat("&itemsPerPage=").concat(String.valueOf(itemsPerPage)).concat("&contracts=").concat(contracts).concat("&locationKeys=").concat(locationKeys).concat("&minDailySalary").concat(String.valueOf(minDailySalary));
+    @GetMapping("getJobs/{page}/{itemsPerPage}/{minDailySalary}")
+    public JobPostings getJobPostings(@PathVariable Integer page, @PathVariable Integer itemsPerPage, @PathVariable Integer minDailySalary  ) {
+        String url = "https://www.free-work.com/api/job_postings?page=".concat(String.valueOf(page)).concat("&itemsPerPage=").concat(String.valueOf(itemsPerPage)).concat("&contracts=").concat("contractor").concat("&locationKeys=").concat("fr~ile-de-france~~").concat("&minDailySalary").concat(String.valueOf(minDailySalary));
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("accept-language", "fr");
